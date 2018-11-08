f44a81f14ec4898f9c29cff1dd81d69251af5035
kocucc789 <49758@naver.com>
2018-11-08 14:09:37 +0900

    2018.11.08
    def함수사용. 그안에 if-else조건문사용.

---
+++ 2-4.py
@@ -0,0 +1,24 @@
+def mymax(a,b):
+    if a>=b:
+        return a
+    else:
+        return b
+
+def mymin(a,b):
+    if a<=b:
+        return a
+    else:
+        return b
+
+def myabs(a):
+    if a>=0:
+        return a
+    else:
+        return -a
+
+    
+    
+    
+    
+
+    
