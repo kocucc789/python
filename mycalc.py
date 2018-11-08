63635411550b9ec663f2b9ebd9d86b3809a15e23
kocucc789 <49758@naver.com>
2018-11-08 14:17:11 +0900

    2018.11.08
    def함수사용.
    if-else사용.
    mycalc.py

---
+++ mycalc.py
@@ -0,0 +1,22 @@
+def mymax(a,b):
+    if a>=b:
+        return a
+    else:
+        return b
+
+def mymin(a,b):
+    if a<=b:
+        return a
+    else:
+        return b
+
+def myabs(a):
+    if a>=0:
+        return a
+    else:
+        return -a
+    
+    
+    
+
+    
