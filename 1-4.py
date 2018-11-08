ae56430a80a946d0d7ea6299bd2043fec6ad6c11
kocucc789 <49758@naver.com>
2018-11-08 13:58:36 +0900

    2018.11.08
    import random.
    삼각형의 면적구하기.

--- 1-4.py
+++ 1-4.py
@@ -1,10 +1,10 @@
 import random
 inside=0
 n=int(input("반복횟수는"))
-for i in range(10000):
+for i in range(n):
     x=random.random()
     y=random.random()
     if y<=x:
         inside+=1
 
-print("삼각형의 면적은",inside/10000*1)        
+print("삼각형의 면적은",inside/n*1)        
